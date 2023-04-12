e91e988e4ab242538330c67a7fe9bb94460c8d15
Mwailongo <toneevin07@gmail.com>
2023-04-12 17:09:59 +0300

    No  Character update

--- projects/angular-utils-lib/src/lib/directive/no-special-characters.directive.ts
+++ projects/angular-utils-lib/src/lib/directive/no-special-characters.directive.ts
@@ -5,14 +5,15 @@ import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 })
 export class NoSpecialCharactersDirective {
     @Input() acceptChars: string = '';
-
-    private regex: RegExp = new RegExp(/^[a-zA-Z0-9]+$/);
+    
+    private regex: RegExp = new RegExp(/^[a-zA-Z0-9]+(?:\s[a-zA-Z0-9]+)*$/);
 
     constructor(private el: ElementRef) { }
 
     @HostListener('keydown', ['$event']) onKeyDown(event: KeyboardEvent) {
         if (this.acceptChars !== '') {
-            this.regex = new RegExp(`^[a-zA-Z0-9${this.acceptChars}]+$`);
+            // invalidates space as first and last character
+            this.regex = new RegExp(`^[a-zA-Z0-9${this.acceptChars}]+(?:\s[a-zA-Z0-9]+)*$`);
         }
 
         if (!this.regex.test(event.key)) {
